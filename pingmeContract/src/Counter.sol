321ba7f8233c55b06c866026073b42a3810733f2
manoahLinks <manoahluka@gmail.com>
2025-10-23 03:35:38 +0100

    feat: work on server

--- pingmeContract/src/Counter.sol
+++ pingmeContract/src/Counter.sol
@@ -2,13 +2,18 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
+    event NumberSet(uint256 indexed oldNumber, uint256 indexed newNumber);
+    event NumberIncremented(uint256 indexed oldNumber, uint256 indexed newNumber);
+    uint256 public number = 0;
+
+    function setNumber(uint256 oldNumber, uint256 newNumber) public {
+        emit NumberSet(oldNumber, newNumber);
         number = newNumber;
     }
 
     function increment() public {
+        emit NumberIncremented(number, number + 1);
         number++;
     }
 }
